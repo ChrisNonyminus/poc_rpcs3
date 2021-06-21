70c2469b9a6ca0ec6209a8e39e1210680777f6ba
Christian <50080567+ChrisNonyminus@users.noreply.github.com>
2021-06-21 09:21:45 -0600

    fix savestate manager

--- rpcs3/Vanguard/ManagedWrapper.cpp
+++ rpcs3/Vanguard/ManagedWrapper.cpp
@@ -6,7 +6,7 @@
 extern "C" __declspec(dllexport) unsigned char ManagedWrapper_peekbyte(long long addr)
 {
 	if (vm::check_addr(static_cast<u32>(addr)) == false)
-		return 0;
+		return 0x58; //unmapped memory; denote this by capital Xs
 	return vm::g_sudo_addr[static_cast<u32>(addr)];
 }
 
@@ -17,7 +17,7 @@ extern "C" __declspec(dllexport) void ManagedWrapper_pokebyte(long long addr, un
 	vm::g_sudo_addr[static_cast<u32>(addr)] = val;
 }
 
-extern "C" __declspec(dllexport) const char* ManagedWrapper_savesavestate(const char* filename)
+extern "C" __declspec(dllexport) void ManagedWrapper_savesavestate(const char* filename)
 {
 	const std::string path = fs::get_cache_dir() + "/savestates/" + (Emu.GetTitleID().empty() ? Emu.GetBoot().substr(Emu.GetBoot().find_last_of(fs::delim) + 1) : Emu.GetTitleID()) + ".SAVESTAT";
 	if (fs::exists(path))
@@ -26,11 +26,11 @@ extern "C" __declspec(dllexport) const char* ManagedWrapper_savesavestate(const 
 	}
 	Emu.Stop(true, true);
 	fs::copy_file(path, std::string(filename), true);
-	return path.c_str();
 }
 
 extern "C" __declspec(dllexport) void ManagedWrapper_loadsavestate(const char* filename)
 {
+	Emu.Stop();
 	Emu.BootGameInState(std::string(filename));
 }
 
